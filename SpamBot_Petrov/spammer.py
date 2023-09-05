6291e34b166885ea41811fe6e90c583c25606c24
denzhhuta <115723596+denzhhuta@users.noreply.github.com>
2023-09-05 21:29:21 +0200

    Added auto-groups into account
    I have added a function that allows me to automatically join all specified chats. It simplifies the work.

--- SpamBot_Petrov/spammer.py
+++ SpamBot_Petrov/spammer.py
@@ -2,29 +2,51 @@ import telethon
 from telethon import TelegramClient, events
 from telethon.events import ChatAction
 from telethon.tl.types import PeerChannel
-from telethon import functions, types
+from telethon.tl import types
+from addition import logs_handler, send_telegram_announcement
+from telethon.tl.functions.channels import JoinChannelRequest
+from python_socks import ProxyType
 import asyncio
 import csv
 import random
 import threading
-from addition import logs_handler, send_telegram_announcement
 import os
-from python_socks import ProxyType
-
+import time
+import logging
 
 chat_listen_to = []
 comments = []
 comments_used = []
-
-
 bot_banned_chat = {}
 client_lock = asyncio.Lock()
+last_response_time = {}
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s [%(levelname)s]: %(message)s',
+    handlers=[
+        logging.FileHandler('spammer.log'),
+        logging.StreamHandler()
+    ]
+)
+
+# Create a logger instance
+logger = logging.getLogger('spammer')
 
 
 accounts = [
-    {"api_id": "24607193", "api_hash": "75e0b0af6c73a423bb12bc1ee0e9c14c", "session": "session_01", "name": "Andrey_bot"}, #Andrey
-    {"api_id": "23255984", "api_hash": "fca003b16ca72a7ca5ddb6762985e3c2", "session": "session_02", "name": "Angelo_bot"} #Max
+    {"api_id": "20600849", "api_hash": "41b4269e451bb95f0a2bfdd61d52947e", "session": "join_groups_1", "name": "Andrey_bot"}#Andrey
+    #{"api_id": "23255984", "api_hash": "fca003b16ca72a7ca5ddb6762985e3c2", "session": "session_02", "name": "Angelo_bot"} #Max
 ]
+
+group_ids = ['https://t.me/ukraina_novosti', 
+             'https://t.me/deniszhhuta_testchannel_3', 
+             'https://t.me/topor', 'https://t.me/toplesofficial', 
+             'https://t.me/nemorgenshtern', 'https://t.me/csgo_main', 
+             'https://t.me/pda4_forum', 'https://t.me/kdrabotnikov', 
+             'https://t.me/moscowmap', 'https://t.me/truexanewsua', 
+             'https://t.me/ca11_you']
 
 
 with open('/Users/zgutadenis/Desktop/Work/CopyBot_Petrov/chats.csv', 'r') as file:
@@ -40,11 +62,38 @@ print(chat_listen_to)
 with open('/Users/zgutadenis/Desktop/Work/SpamBot_Petrov/messages.csv', 'r') as file:
     csvreader = csv.reader(file, delimiter='/')
     for row in csvreader:
-        if row:  # Check if the row is not empty
+        if row:
             message_text = row[0]
             comments.append(message_text.strip())
 
-       
+async def join_groups_for_account(account):
+    proxy = {
+        'proxy_type': ProxyType.SOCKS5,
+        'addr': "45.56.124.180",
+        'port': 27614,
+        'username': "modeler_2B6ifs",
+        'password': "aNIxmx0U6huI",
+        'rdns': True
+        }
+    async with TelegramClient(account['session'], account['api_id'], account['api_hash']) as client_1:
+        await client_1.start()
+        await client_1.connect()   
+        for group_id in group_ids:
+            try:
+                await client_1(JoinChannelRequest(group_id))
+                logger.info(f"Join group/channel with ID: {group_id} for {account['name']}")
+                await asyncio.sleep(4)
+            except Exception as e:
+                 logger.error(f"Error joining group/channel with ID: {group_id} for {account['name']}: {str(e)}")
+                 await asyncio.sleep(3)
+                        
+        print(f"All requested groups/channels were joined for {account['name']}")
+        await asyncio.sleep(2)
+        await client_1.disconnect()
+    
+async def join_chats():
+    for account in accounts:
+        await join_groups_for_account(account)
 
 API_ID = "20600849"
 API_HASH = '41b4269e451bb95f0a2bfdd61d52947e'
@@ -64,17 +113,20 @@ async def send_message_from_random_account(chat_id, random_comment, reply_to_mes
                 'password': "aNIxmx0U6huI",
                 'rdns': True
             }
-            async with TelegramClient(random_account['session'], random_account['api_id'], random_account['api_hash'], proxy=proxy) as client:
+            async with TelegramClient(random_account['session'], random_account['api_id'], random_account['api_hash']) as client:
                 client.start()
                 client.connect()
+                
+                name_bot = [str(random_account["name"]).strip()]
                 
                 # Check if the account is banned before sending a message
                 if chat_id in bot_banned_chat:
                     banned_bot_names = {bot['name'] for bot in bot_banned_chat[chat_id]}
                     if banned_bot_names == {account['name'] for account in accounts}:
+                        await send_telegram_announcement(chat_entity.title, chat_id, name_bot, 2)
                         print(f"All bots are banned for chat {chat_id}. Stopping the loop.")
                         client.disconnect()
-                        return  # Break the loop
+                        return
                 
                     if any(bot['name'] == random_account['name'] for bot in bot_banned_chat[chat_id]):
                         print(f"Bot '{random_account['name']}' is banned for chat {chat_id}")
@@ -89,7 +141,6 @@ async def send_message_from_random_account(chat_id, random_comment, reply_to_mes
                 chat_id_converted = [str(chat_id).strip()]
                 message_id_converted = [str(reply_to_message_id).strip()]
                 filename = os.path.join("/Users/zgutadenis/Desktop/Work/SpamBot_Petrov", "logs.csv")
-                name_bot = [str(random_account["name"]).strip()]
                 
                 try:
                     print(chat_id, "HERE")
@@ -99,7 +150,7 @@ async def send_message_from_random_account(chat_id, random_comment, reply_to_mes
                 except telethon.errors.rpcerrorlist.ChannelPrivateError as e:
                     try:
                         print("Channel is private or bot is banned from the channel:", chat_entity.title, "with ID:", chat_id)
-                        await send_telegram_announcement(chat_entity.title, chat_id, name_bot)
+                        await send_telegram_announcement(chat_entity.title, chat_id, name_bot, 1)
                         comments_used.remove(random_comment)
                         
                         new_banned_entry = {
@@ -144,8 +195,8 @@ async def send_message_from_random_account(chat_id, random_comment, reply_to_mes
                 
                 finally:
                     client.disconnect()
+                    await asyncio.sleep(30)
                     break
-
 
 
 
@@ -163,16 +214,59 @@ async def new_message_handler(event):
                 comments_used.append(random_comment)
                 print(len(comments_used))
                 print(len(comments))
-                delay_seconds = random.uniform(5, 10)
+
+                # Calculate the delay based on the last response time or use a default delay
+                last_response = last_response_time.get(event.chat_id)
+                if last_response is not None:
+                    time_since_last_response = asyncio.get_event_loop().time() - last_response
+                    if time_since_last_response < 30:
+                        delay_seconds = random.uniform(30, 50)
+                    else:
+                        delay_seconds = random.uniform(30, 50)
+                else:
+                    delay_seconds = random.uniform(30, 50)
+
                 await asyncio.sleep(delay_seconds)
 
                 chat_entity = await client.get_entity(event.chat_id)
                 await send_message_from_random_account(event.chat_id, random_comment, message_to_comment.id, chat_entity)
+                last_response_time[event.chat_id] = asyncio.get_event_loop().time()  # Update last response time
+
                 break
 
             else:
                 print("Comment is already used. Skipping...")
-          
-                                  
-client.start()
-client.run_until_disconnected()
+
+
+if __name__ == "__main__":
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(join_chats())
+
+
+    client.start()
+    client.run_until_disconnected()
+
+# @client.on(events.NewMessage(chats=chat_listen_to))
+# async def new_message_handler(event):
+#     message_to_comment = event.message
+#     print("Received message:", message_to_comment.text)
+
+#     if message_to_comment.chat_id and message_to_comment.replies is not None:
+#         if len(comments_used) == len(comments):
+#             raise Exception("All comments have been used!")
+
+#         for random_comment in comments:
+#             if random_comment not in comments_used:
+#                 comments_used.append(random_comment)
+#                 print(len(comments_used))
+#                 print(len(comments))
+#                 delay_seconds = random.uniform(30, 50)
+#                 await asyncio.sleep(delay_seconds)
+
+#                 chat_entity = await client.get_entity(event.chat_id)
+#                 await send_message_from_random_account(event.chat_id, random_comment, message_to_comment.id, chat_entity)
+#                 #await asyncio.sleep(30)
+#                 break
+
+#             else:
+#                 print("Comment is already used. Skipping...")
